e802ab989fa031f2db6cfbc516525ed17bbe14c0
dilicat0813 <mkh0813@naver.com>
2023-06-23 01:26:33 +0900

    수정

--- 6.Subscribe_extract/subscribe.py
+++ 6.Subscribe_extract/subscribe.py
@@ -11,9 +11,9 @@ if not os.path.exists(folder_path):
 file_path = os.path.join(folder_path, "subscribe.csv")
 
 #기본 DB불러오기
-total = pd.read_csv("total.csv", index_col=0) #경로/이름 설정
-subscribe = pd.read_csv("subscribe_2023-06-22.csv") #경로/이름 설정
-
+total = pd.read_csv("data/total.csv", index_col=0) #경로/이름 설정
+subscribe = pd.read_csv("data/subscribe.csv") #경로/이름 설정
+subscribe.drop_duplicates(subset="EMAIL", keep='last', inplace=True)
 subscribe.to_csv(file_path, index=False, encoding='utf-8-sig') #RPA 소스 파일 저장 #논리상 위치 여기가 맞음
 DT_Query = total[(total['MUMM_LON_HALFLIFE_CO'] >= 350) & (total['MUMM_LON_HALFLIFE_CO'] <= 6000)] #반감기 데이터 중 이상치 및 상위 5퍼센트 제거
 DT_Query = DT_Query[(DT_Query['LON_CO'] <= 6500)] #누적 대출수 상위 5퍼센트 제거
@@ -33,7 +33,7 @@ for index, row in subscribe.iterrows():
     except:
         srchwrd_list = []
     
-    if srchwrd and srchwrd.strip():
+    if srchwrd:
         DT_Query2 = DT_Query1[
             DT_Query1['TITLE_NM'].str.contains('|'.join(srchwrd_list), regex=True) |
             DT_Query1['AUTHR_NM'].str.contains('|'.join(srchwrd_list), regex=True) |
@@ -43,7 +43,7 @@ for index, row in subscribe.iterrows():
         ]
         DT_Query2 = DT_Query2.drop_duplicates()
     
-        if len(DT_Query2) > 0: #예외처리 (검색한 대상이 있을/없을)
+        if len(DT_Query2) > 0:
             DT_recommend = DT_Query2.sample(5)
         else:
             DT_recommend = DT_Query1.sample(5)
