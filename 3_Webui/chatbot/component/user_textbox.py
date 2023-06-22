6b16810fa6636009e280b94699a478280e331a86
Jonghyunchoi940905 <131318173+Jonghyunchoi940905@users.noreply.github.com>
2023-06-22 14:37:58 +0900

    add css
    transform transrate, border bottom dotted

--- 3_Webui/chatbot/component/user_textbox.py
+++ 3_Webui/chatbot/component/user_textbox.py
@@ -2,14 +2,14 @@ import dash_bootstrap_components as dbc
 
 def user_textbox(text, box="other"):
     style = {
-        "max-width": "75%",
         "width": "max-content",
         "padding": "10px 15px",
         "border-radius": "25px",
         "font-size":"large",
         "font-family":"sans-serif",
         "margin-bottom":"25px",
-        "font-weight":"bold"
+        "font-weight":"bold",
+        "transform":"translate(0%, 18%)"
     }
 
     color = "yellow"
