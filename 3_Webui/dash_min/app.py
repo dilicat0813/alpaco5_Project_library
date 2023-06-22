b1cd5b50a3d3f2d6401fa36aeb6c1b7039ba71ea
dilicat0813 <mkh0813@naver.com>
2023-06-22 09:49:01 +0900

    구독서비스 sql 연동

--- 3_Webui/dash_min/app.py
+++ 3_Webui/dash_min/app.py
@@ -1,7 +1,8 @@
-
+import pymysql
 import pandas as pd
 import dash
 from dash import Dash, dcc, html, Input, Output, State
+
 
 ALLOWED_TYPES = ("email", "text")
 subscribe = pd.DataFrame(columns=["EMAIL", "KDC", "SRCHWRD"])
@@ -85,92 +86,43 @@ def cb_render(n_clicks, email_value, text_value, dropdown_value):
         }
         df = pd.DataFrame(data, index=[0])
         subscribe = pd.concat([subscribe, df], ignore_index=True)
-        subscribe.to_csv("test.csv", index=False)  # 데이터프레임을 CSV 파일에 추가
+        subscribe.to_csv("subscribe.csv", index=False)  # 데이터프레임을 CSV 파일에 추가
         print(subscribe)
-
-# import pandas as pd
-# import dash
-# from dash import Dash, dcc, html, Input, Output, State
-
-# ALLOWED_TYPES = ("email", "text")
-# subscribe = pd.DataFrame(columns=["EMAIL", "KDC", "SRCHWRD"])
-
-# app = dash.Dash(__name__)
-
-# kdc_dict_total = {
-#     '0': '총류',
-#     '1': '철학',
-#     '2': '종교',
-#     '3': '사회과학',
-#     '4': '자연과학',
-#     '5': '기술과학',
-#     '6': '예술',
-#     '7': '언어',
-#     '8': '문학',
-#     '9': '역사',
-# }
-
-# app.layout = html.Div(
-#     [
-#         html.Div(
-#             [
-#                 dcc.Input(
-#                     id="input_{}".format("email"),
-#                     type="email",
-#                     placeholder="{} 입력하세요".format("등록할 Email을"),
-#                     style={"height": "30px", "font-size": "16px","margin-right": "15px"}
-#                 ),
-#                 dcc.Input(
-#                     id="input_{}".format("text"),
-#                     type="text",
-#                     placeholder="{} 입력하세요".format("검색어를"),
-#                     style={"height": "30px", "font-size": "16px"}
-#                 ),
-#             ],
-#             style={"margin-bottom": "20px"},
-#         ),
         
-#         dcc.Dropdown(
-#             id="dropdown",
-#             options=[
-#                 {"label": kdc_dict_total[kdc], "value": kdc}
-#                 for kdc in kdc_dict_total
-#             ],
-#             value='0',  # 기본 선택값을 '총류'로 설정
-#             placeholder="분류를 선택하세요",
-#             style={"height": "30px", "width": "420px", "font-size": "16px"},
-#         ),
-#         html.Button("구독하기", id="button", n_clicks=0,
-#                      style={"height": "30px", "font-size": "16px", "margin-left": "150px", "margin-top": "30px"}
-#                      ),
-#         html.Div(id="out-all-types"),
-#     ],
-#     style={"margin-top": "50px"},
-# )
+        # Connect to MySQL #OS사용하지 않음 (일단 단순제작)
+        conn = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='1234',
+            db='library',
+            charset='utf8'
+        )
+        tablename ="Subscribe"
 
+        # Create table if it doesn't exist
+        sql = f'''CREATE TABLE IF NOT EXISTS {tablename} (  
+                EMAIL varchar(255),
+                KDC varchar(255),
+                SRCHWRD varchar(255)
+                )
+              '''
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            conn.commit()
 
+        # Insert data into the table
+        with conn.cursor() as cur:
+            for _, row in subscribe.iterrows():
+                email = row['EMAIL']
+                kdc = row['KDC']
+                srchwrd = row['SRCHWRD']
+                sql = f"INSERT INTO {tablename} (EMAIL, KDC, SRCHWRD) VALUES ('{email}', '{kdc}', '{srchwrd}')"
+                cur.execute(sql)
+            conn.commit()
 
-# @app.callback(
-#     Output("out-all-types", "children"),
-#     [Input("button", "n_clicks")],
-#     [
-#         State("input_email", "value"),
-#         State("input_text", "value"),
-#         State("dropdown", "value"),
-#     ],
-# )
-# def cb_render(n_clicks, email_value, text_value, dropdown_value):
-#     global subscribe  # 전역 변수로 선언
-#     if n_clicks > 0:
-#         data = {
-#             "EMAIL": email_value,
-#             "KDC": dropdown_value,
-#             "SRCHWRD": text_value,
-#         }
-#         df = pd.DataFrame(data, index=[0])
-#         subscribe = pd.concat([subscribe, df], ignore_index=True)
-#         subscribe.to_csv("test.csv", index=False)  # 데이터프레임을 CSV 파일에 추가
-#         print(subscribe)
+        # Close the connection
+        conn.close()
+        return html.Div("구독을 신청하였습니다.", style={"color": "red", "margin-left": "120px"})
 
 if __name__ == "__main__":
     app.run_server(debug=True)
