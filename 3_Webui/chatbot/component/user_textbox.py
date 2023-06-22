a1e847408bc2e2408b784c69f1d78ce51f5bb660
Jonghyunchoi940905 <131318173+Jonghyunchoi940905@users.noreply.github.com>
2023-06-22 11:09:48 +0900

    font modified
    font modified

--- 3_Webui/chatbot/component/user_textbox.py
+++ 3_Webui/chatbot/component/user_textbox.py
@@ -2,11 +2,15 @@ import dash_bootstrap_components as dbc
 
 def user_textbox(text, box="other"):
     style = {
-        "max-width": "55%",
+        "max-width": "75%",
         "width": "max-content",
         "padding": "10px 15px",
         "border-radius": "25px",
+        "font-size":"large",
+        "font-family":"sans-serif",
+        "margin-bottom":"25px"
     }
+    
     color = "light"
     
     return dbc.Card(text, style=style, body=True, color=color)
