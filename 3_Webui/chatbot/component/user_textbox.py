ee4a5406c11d26ee765ec34dce5ec37df2e705c3
Jonghyunchoi940905 <131318173+Jonghyunchoi940905@users.noreply.github.com>
2023-06-22 17:50:18 +0900

    css modified
    modified

--- 3_Webui/chatbot/component/user_textbox.py
+++ 3_Webui/chatbot/component/user_textbox.py
@@ -2,14 +2,15 @@ import dash_bootstrap_components as dbc
 
 def user_textbox(text, box="other"):
     style = {
-        "width": "max-content",
+        "width":"fit-content",
+        "max-width":"50vw",
         "padding": "10px 15px",
         "border-radius": "25px",
         "font-size":"large",
         "font-family":"sans-serif",
         "margin-bottom":"25px",
         "font-weight":"bold",
-        "transform":"translate(0%, 18%)"
+        "transform":"translate(0%, 0%)"
     }
 
     color = "yellow"
